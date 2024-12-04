8802624186742ba892382294258826e3debf39e4
VitorGouveia <vitorneves.gouveia10@gmail.com>
2024-12-04 12:07:00 -0300

    1.11.1: add faq q&a

--- components/Faq.tsx
+++ components/Faq.tsx
@@ -2,29 +2,106 @@ import { ArrowDownIcon } from "lucide-react"
 
 const questions: Array<{ question: string; answer: string }> = [
   {
-    question: "Como serão realizadas as cobranças das mensalidades",
+    question: "Como funciona o curso de redação para o ENEM?",
     answer:
-      "Todos os meses, no dia em que realizou a compra da primeira mensalidade, chegará uma nova mensalidade para pagamento na plataforma do aluno",
+      "Nosso curso oferece aulas presenciais, videoaulas, materiais de apoio e correção de redações com base nos critérios oficiais do ENEM",
   },
   {
-    question: "Como serão realizadas as cobranças das mensalidades",
+    question: "Qual é o nível de suporte oferecido aos alunos?",
     answer:
-      "Todos os meses, no dia em que realizou a compra da primeira mensalidade, chegará uma nova mensalidade para pagamento na plataforma do aluno",
+      "Oferecemos suporte via whatsapp ou e-mail para tirar dúvidas, além de acompanhamento personalizado com a professora de redação.",
   },
   {
-    question: "Como serão realizadas as cobranças das mensalidades",
+    question: "As correções são feitas por professores especializados?",
     answer:
-      "Todos os meses, no dia em que realizou a compra da primeira mensalidade, chegará uma nova mensalidade para pagamento na plataforma do aluno",
+      "Sim, todas as redações são corrigidas por professores com experiência em preparação para o ENEM, levando em consideração os cinco critérios da matriz de avaliação.",
   },
   {
-    question: "Como serão realizadas as cobranças das mensalidades",
+    question: "O curso aborda temas de redação anteriores do ENEM?",
     answer:
-      "Todos os meses, no dia em que realizou a compra da primeira mensalidade, chegará uma nova mensalidade para pagamento na plataforma do aluno",
+      "Sim, analisamos e explicamos temas de redações de anos anteriores, além de oferecer sugestões de temas prováveis para o próximo exame.",
   },
   {
-    question: "Como serão realizadas as cobranças das mensalidades",
+    question:
+      "O curso é adequado para quem tem dificuldades em escrever redações?",
     answer:
-      "Todos os meses, no dia em que realizou a compra da primeira mensalidade, chegará uma nova mensalidade para pagamento na plataforma do aluno",
+      "Sim! Temos aulas passo a passo para construir uma redação nota 1000 para alunos de qualquer nível.",
+  },
+  {
+    question: "O curso oferece simulados de redação?",
+    answer:
+      "Sim, realizamos simulados periódicos com temas inéditos para que você treine suas habilidades e fique preparado para o dia do exame.",
+  },
+  {
+    question: "Posso acessar o curso pelo celular?",
+    answer:
+      "Sim, nossa plataforma pode ser acessada por qualquer dispositivo com internet, incluindo celulares e tablets.",
+  },
+  {
+    question: "Como faço para me inscrever?",
+    answer:
+      'Basta clicar no botão "Matricule-se", escolher o plano de sua preferência e realizar o pagamento. Em seguida, você sera redirecionado à plataforma.',
+  },
+  {
+    question: "Qual é a duração do curso?",
+    answer:
+      "O acesso ao curso é válido até a data do ENEM, permitindo que você estude no seu ritmo.",
+  },
+  {
+    question: "Há uma política de reembolso?",
+    answer:
+      "Sim, oferecemos reembolso total em até 7 dias após a compra, caso você não fique satisfeito com o curso.",
+  },
+  {
+    question: "Até quando posso usar meu plano?",
+    answer:
+      "Nossos planos são pré-pagos e tem validade de 1 mes. Após esse período ele deve ser renovado.",
+  },
+  {
+    question: "Preciso ter algum conhecimento prévio para fazer o curso?",
+    answer:
+      "Não, o curso é adequado tanto para iniciantes quanto para quem já tem experiência em redação e quer aprimorar suas habilidades.",
+  },
+  {
+    question: "O curso aborda temas atuais e possíveis para o ENEM 2024?",
+    answer:
+      "Sim, além de analisarmos temas passados, abordamos temas atuais e questões que podem ser relevantes para o próximo ENEM.",
+  },
+  {
+    question: "Quais são os métodos de pagamento disponíveis?",
+    answer:
+      "Aceitamos pagamentos via cartão de crédito, boleto bancário e Pix.",
+  },
+  {
+    question: "O curso oferece materiais de apoio?",
+    answer:
+      "Sim, disponibilizamos apostilas, tempestade de ideias(que ajudam a produzir sua redação) e exercícios complementares para reforçar o aprendizado.",
+  },
+  {
+    question:
+      "Como posso melhorar minha nota de redação no ENEM com este curso?",
+    answer:
+      "Nossas correções detalhadas, simulados e materiais específicos te ajudarão a entender onde você pode melhorar e a atingir uma nota mais alta.",
+  },
+  {
+    question:
+      "Vocês ensinam técnicas específicas para cada critério de correção do ENEM?",
+    answer:
+      "Sim, nossas aulas são divididas por critérios de correção, como coesão, coerência, argumentação, domínio da norma culta e proposta de intervenção.",
+  },
+  {
+    question: "O curso tem garantia de aprovação?",
+    answer:
+      "Embora não possamos garantir aprovação, fornecemos todas as ferramentas e suporte necessários para maximizar seu desempenho na redação do ENEM.",
+  },
+  {
+    question: "Qual é a duração média de cada aula?",
+    answer: "As aulas duram 90 minutos.",
+  },
+  {
+    question: "O que acontece se eu precisar cancelar meu plano?",
+    answer:
+      "Você pode cancelar a sua matrícula a qualquer momento sem custos adicionais.",
   },
 ]
 
