c4298c05e2870038207944b24105e2f8a266c79b
VitorGouveia <vitorneves.gouveia10@gmail.com>
2025-01-13 13:21:08 -0300

    1.14.2: update discount messages

--- components/PreEnrollmentSection.tsx
+++ components/PreEnrollmentSection.tsx
@@ -10,11 +10,12 @@ export function PreEnrollmentCard({
   return (
     <Div className="flex w-full flex-col items-center justify-center gap-6 rounded-md bg-green-200 px-8 py-8 lg:py-32">
       <h2 className="text-center text-2xl font-bold leading-normal text-zinc-800 lg:text-4xl">
-        Pré-Matrículas 2025 Com 20% de Desconto!
+        Pré-Matrículas 2025 Com 10% de Desconto!
       </h2>
 
       <p className="text-center text-xl text-zinc-600">
-        Garanta 20% de desconto na Pré-Matrícula de 2025 apenas em dezembro.
+        Garanta 10% de desconto na Pré-Matrícula de 2025 apenas no mês de
+        janeiro.
       </p>
 
       <p className="text-center text-xl text-zinc-600">
