a98b8a12f52705c338c6b650b40664e447ee7e58
VitorGouveia <vitorneves.gouveia10@gmail.com>
2025-03-17 21:34:58 -0300

    add 'referralId' to cookies if present in query params

---
+++ middleware.ts
@@ -0,0 +1,22 @@
+import { NextRequest, NextResponse } from "next/server"
+
+export function middleware(req: NextRequest) {
+  const url = req.nextUrl
+  const referralId = url.searchParams.get("referralId")
+
+  const res = NextResponse.next()
+
+  if (referralId) {
+    res.cookies.set("referralId", referralId, {
+      path: "/",
+      maxAge: 60 * 60 * 24 * 7, // 7 days
+    })
+  }
+
+  return res
+}
+
+// Apply middleware to all routes
+export const config = {
+  matcher: "/:path*",
+}
